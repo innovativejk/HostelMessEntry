ab3fc0c1f427d3cf64250d21097a7df949c4aa53
JAY KUMAR <jk7078962@gmail.com>
2025-06-01 17:17:52 +0530

    main

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+// postcss.config.js
+import tailwindcss from '@tailwindcss/postcss'; // <-- यह लाइन बदली गई है
+import autoprefixer from 'autoprefixer';
+
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    [tailwindcss]: {}, // <-- यह लाइन बदली गई है, अब यह आयातित प्लगइन का उपयोग करती है
+    [autoprefixer]: {}, // <-- यह भी स्पष्टता के लिए बदल दिया गया है
   },
 };
